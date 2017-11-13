055e13772e57cbecf7f5c2a08e67afc5343b326c
yurina shin <yurinas@gmail.com>
2017-11-13 19:09:00 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,3 @@
 
 def fizzbuzz(number)
-  if number / 3
-    puts "Fizz"
-  elseif number / 5
-  puts "Buzz"
-else number / 3 and 5
-  puts "Fizzbuzz"
 end
-fizzbuzz (3)
-fizzbuzz (5)
-fizzbuzz (15)
