6b7dbb463958c506be035d5ed6b48e25aca1ba10
yurina shin <yurinas@gmail.com>
2017-11-13 20:26:05 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,12 @@
 
 def fizzbuzz(number)
-  if number % 3 == 0
+if number % 15 == 0
+  "Fizzbuzz"
+elsif number % 3 == 0
       "Fizz"
     elsif number % 5 == 0
 "Buzz"
-elsif number % 15 == 0
-  "Fizzbuzz"
+
 
 end
 end
